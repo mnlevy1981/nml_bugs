bab4ff7ce6432a8df2ef23a7ff4ed6bf8a639f1d
Michael Levy <mike.levy.work@gmail.com>
2016-01-05 16:13:46 -0700

    Work around for reading nml from array
    The way we store namelists in each element of the nl_buffer array, it's
    possible to determine which element element of the array actually
    contains the namelist of interest and only read that element. This
    avoids the silent error from gfortran.

--- namelist.F90
+++ namelist.F90
@@ -6,10 +6,12 @@ Program my_nml_prog
   logical, parameter :: io_msg_on = .false.
   !logical, parameter :: debug_on = .true.
   logical, parameter :: debug_on = .false.
+  !logical, parameter :: verbose = .true.
+  logical, parameter :: verbose = .false.
   integer, parameter :: max_lines = 255, char_len=255
 
   integer :: a, b, c, ierr, nml_in, i
-  character(len=char_len) :: nl_string, msg
+  character(len=char_len) :: nl_string, msg, tmp_str
   character(len=char_len), dimension(max_lines) :: nl_buffer
 
   namelist /my_nml1/ a, b, c
@@ -48,7 +50,15 @@ Program my_nml_prog
   c = 0
   read(nl_string, nml=my_nml1, iostat=ierr, iomsg=msg)
   call print_err(ierr, msg, header="Read my_nml1")
-  write(*,my_nml1)
+  if (verbose.or.debug_on) &
+    write(*,my_nml1)
+  if (any((/a,b,c/).eq.0)) then
+    print*, "Read from string: At least one namelist variable is still 0"
+    print*, "                  FAIL!"
+  else
+    print*, "Read from string: Successful namelist read!"
+    print*, "                  PASS!"
+  end if
 
   ! Read second namelist from array, each element containing single namelist
   a = 0
@@ -57,11 +67,21 @@ Program my_nml_prog
   ierr = 1
   i = 1
   do while ((ierr.ne.0).and.(i.le.max_lines))
-    read(nl_buffer(i), nml=my_nml2, iostat=ierr, iomsg=msg)
+    tmp_str = nl_buffer(i)
+    if (tmp_str(2:8).eq."my_nml2") &
+      read(nl_buffer(i), nml=my_nml2, iostat=ierr, iomsg=msg)
     i=i+1
   end do
   call print_err(ierr, msg, header="Read my_nml2")
-  write(*,my_nml2)
+  if (verbose.or.debug_on) &
+    write(*,my_nml2)
+  if (any((/a,b,c/).eq.0)) then
+    print*, "Read from array: At least one namelist variable is still 0"
+    print*, "                 FAIL!"
+  else
+    print*, "Read from array: Successful namelist read!"
+    print*, "                 PASS!"
+  end if
 
 contains
 
