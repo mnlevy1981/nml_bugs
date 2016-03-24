ce5e952effa7a504e4ec432bcd1596f8de119076
Michael Levy <mike.levy.work@gmail.com>
2016-03-24 16:30:03 -0600

    Add array of strings to namelist testing
    Want to see how arrays of strings are treated as members of the namelist, so I
    added a
    
    character(len=15), dimension(2)
    
    variable to the namelists.

--- namelist.F90
+++ namelist.F90
@@ -11,11 +11,12 @@ Program my_nml_prog
   integer, parameter :: max_lines = 255, char_len=255
 
   integer :: a, b, c, ierr, nml_in, i
+  character(len=15), dimension(2) :: d
   character(len=char_len) :: nl_string, msg, tmp_str
   character(len=char_len), dimension(max_lines) :: nl_buffer
 
-  namelist /my_nml1/ a, b, c
-  namelist /my_nml2/ a, b, c
+  namelist /my_nml1/ a, b, c, d
+  namelist /my_nml2/ a, b, c, d
 
   ! Initialize string and array to be empty
   nl_string = ''
@@ -48,6 +49,7 @@ Program my_nml_prog
   a = 0
   b = 0
   c = 0
+  d = (/'Hello', 'World'/)
   read(nl_string, nml=my_nml1, iostat=ierr, iomsg=msg)
   call print_err(ierr, msg, header="Read my_nml1")
   if (verbose.or.debug_on) &
